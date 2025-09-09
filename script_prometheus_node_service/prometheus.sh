f95fd671c85aa40676aa09a016ecafdda35d1ba6
sate-antaranyan-sada <225000704+sate-antaranyan-sada@users.noreply.github.com>
2025-09-09 16:43:13 +0400

    Updated prometheus.sh

--- script_prometheus_node_service/prometheus.sh
+++ script_prometheus_node_service/prometheus.sh
@@ -8,16 +8,18 @@ else
 
     wget https://github.com/prometheus/prometheus/releases/download/v3.6.0-rc.0/prometheus-3.6.0-rc.0.linux-amd64.tar.gz
     tar xvf prometheus-3.6.0-rc.0.linux-amd64.tar.gz
-    mv /usr/local/prometheus-3.6.0-rc.0.linux-amd64 /usr/local_prometheus
+    mv prometheus-3.6.0-rc.0.linux-amd64 /usr/local
 
     cat >/usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus.yml<< EOF
     global:
     scrape_interval: 15s
+
     scrape_configs:
     - job_name: prometheus
-    static_configs:
-    - targets: ['localhost:9090']
+        static_configs:
+        - targets: ['localhost:9090']
 EOF
+    sudo mkdir -p /var/lib/premetheus #Data directory
 
     cat >/usr/lib/systemd/system/prometheus.service<< EOF
     [Unit]
@@ -25,15 +27,20 @@ EOF
     After=network.target
 
     [Service]
-    Type=simple
-    ExecStart=/usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus --config.file=/usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus.yml
+    Type=simple 
+    ExecStart=/usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus \
+        --config.file=/usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus.yml \
+        --storage.tsdb.path=/var/lib/prometheus \
+        --web.listen-address=:9090
+    Restart=on-failure
+    RestartSec=2
 
     [Install]
     WantedBy=multi-user.target
 EOF
-
-    sudo useradd -M -r /bin/false prometheus
-    sudo chown -R prometheus. /usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus
+    sudo useradd -r -s /usr/sbin/nologin prometheus
+    sudo chown -R prometheus:prometheus /usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus
+    sudo chown -R prometheus:prometheus /usr/local/prometheus-3.6.0-rc.0.linux-amd64 /var/lib/prometheus
     sudo chmod 755 /usr/local/prometheus-3.6.0-rc.0.linux-amd64/prometheus
     sudo systemctl daemon-reload
     sudo systemctl start prometheus.service
